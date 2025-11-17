4275a959b24c85b8966bbfdebfcac7721a925059
53stones <2028725773@qq.com>
2025-11-17 12:22:35 +0800

    Add files via upload

---
+++ sequence_list_static.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdbool.h>
+#define MAXSIZE 100
+typedef struct
+{
+    int data[MAXSIZE];
+    int length;
+} sequence;
+typedef struct
+{
+    int *data;
+    int length;
+} list;
+void Init_list(list *list) // 动态分配
+{
+    list->data = (int *)malloc(sizeof(int) * MAXSIZE);
+    list->length = 0;
+    return;
+}
+
+void Init_sequence(sequence *list) // 静态分配
+{
+    for (int i = 0; i < MAXSIZE; i++)
+    {
+        list->data[i] = 0;
+    }
+    list->length = 0;
+}
+void Insert_sequence(sequence *list, int i, int n)
+{
+    if (i < 1 || i > list->length + 1)
+        return;
+    if (list->length >= MAXSIZE)
+        return;
+    for (int j = list->length - 1; j >= i - 1; j--)
+    {
+        list->data[j + 1] = list->data[j];
+    }
+    list->data[i - 1] = n;
+    list->length++;
+    return;
+}
+void Delete_sequence(sequence *list, int i)
+{
+    if (i < 1 || i > list->length)
+        return;
+    for (int j = i - 1; j < list->length - 1; j++)
+    {
+        list->data[j] = list->data[j + 1];
+    }
+    list->length--;
+    return;
+}
+void Find_sequence(sequence *list, int n)
+{
+    for (int i = 0; i < list->length; i++)
+    {
+        if (list->data[i] == n)
+        {
+            printf("%d\n", i + 1);
+            return;
+        }
+    }
+    printf("Not found\n");
+    return;
+}
+int Get_value(sequence *list, int i)
+{
+    if (i < 1 || i > list->length)
+        return 0;
+    return list->data[i - 1];
+}
+int Get_length(sequence *list)
+{
+    return list->length;
+}
+bool Is_empty(sequence *list)
+{
+    if (list->length == 0)
+        return true;
+    return false;
+}
+bool Is_full(sequence *list)
+{
+    if (list->length == MAXSIZE)
+        return true;
+    return false;
+}
+void clear_sequence(sequence *list) // 也可以直接将length置为0
+{
+    for (int i = 0; i < list->length; i++)
+    {
+        list->data[i] = 0;
+    }
+    list->length = 0;
+    return;
+}
+void Print_sequence(sequence *list)
+{
+    for (int i = 0; i < list->length; i++)
+    {
+        printf("%d ", list->data[i]);
+    }
+    printf("\n");
+    return;
+}
+int main()
+{
+    sequence list;
+}
---
+++ single_linked_list.c
@@ -0,0 +1,226 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+typedef struct list
+{
+    int data;
+    struct list *next;
+} list;
+bool Init_list(list **mylist)
+{
+    *mylist = (list *)malloc(sizeof(list));
+    if (*mylist == NULL)
+        return false;
+    (*mylist)->next = NULL;
+    (*mylist)->data = 0;
+    return true;
+}
+void Free_list(list *mylist)
+{
+    free(mylist);
+}
+bool Sert_list(list *mylist, int i, int n)
+{
+    if (i < 1 || mylist == NULL)
+        return false;
+    list *p;
+    p = mylist;
+    for (int j = 0; j < i - 1; j++)
+    {
+        p = p->next;
+    } // 指向插入前的位置
+    if (p == NULL)
+        return false;
+    list *s = (list *)malloc(sizeof(list));
+    s->data = n;
+    s->next = p->next;
+    p->next = s;
+    return true;
+}
+bool Sert_list_front(list *mylist, int n)
+{
+    if (mylist == NULL)
+        return false;
+    list *p = mylist;
+    list *q = (list *)malloc(sizeof(list));
+    if (q == NULL)
+        return false;
+    q->data = n;
+    q->next = p->next;
+    p->next = q;
+    return true;
+}
+bool Sert_list_behind(list *mylist, int n)
+{
+    if (mylist == NULL)
+        return false;
+    list *p = mylist;
+    list *q = (list *)malloc(sizeof(list));
+    if (q == NULL)
+        return false;
+    q->data = n;
+    while (p->next != NULL)
+    {
+        p = p->next;
+    }
+    p->next = q;
+    q->next = NULL;
+    return true;
+}
+bool Delete_list(list *mylist, int i)
+{
+    if (i < 1 || mylist == NULL)
+        return false;
+    list *p = mylist;
+    for (int j = 0; j < i - 1; j++)
+    {
+        if (p == NULL)
+            return false;
+        p = p->next;
+    }
+    if (p == NULL || p->next == NULL)
+        return false;
+    list *s = p->next;
+    p->next = s->next;
+    free(s);
+    return true;
+}
+bool Delete_list_front(list *mylist)
+{
+    if (mylist == NULL || mylist->next == NULL)
+        return false;
+    list *p = mylist->next;
+    mylist->next = p->next;
+    free(p);
+    return true;
+}
+bool Delete_list_behind(list *mylist)
+{
+    if (mylist == NULL)
+        return false;
+    list *p = mylist;
+    list *q = p;
+    while (p->next != NULL)
+    {
+        q = p;
+        p = p->next;
+    }
+    q->next = NULL;
+    free(p);
+    return true;
+}
+bool Find_value_list(list *mylist, int n)
+{
+    if (mylist == NULL)
+        return false;
+    list *p = mylist;
+    while (p->next != NULL)
+    {
+        if (p->next->data == n)
+            return true;
+        p = p->next;
+    }
+    return false;
+}
+bool Find_index_list(list *mylist, int i, int *e)
+{
+    if (i < 1 || mylist == NULL)
+        return false;
+    list *p = mylist;
+    for (int j = 0; j < i; j++)
+    {
+        if (p == NULL)
+            return false;
+        p = p->next;
+    }
+    if (p == NULL)
+        return false;
+    *e = p->data;
+    return true;
+}
+bool Modify_value_list(list *mylist, int n, int m)
+{
+    if (mylist == NULL)
+        return false;
+    list *p = mylist;
+    while (p->next != NULL)
+    {
+        if (p->next->data == n)
+        {
+            p->next->data = m;
+            return true;
+        }
+        p = p->next;
+    }
+    return false;
+}
+bool Modify_index_list(list *mylist, int i, int n)
+{
+    if (mylist == NULL || i < 1)
+        return false;
+    list *p = mylist;
+    for (int j = 0; j < i; j++)
+    {
+        if (p == NULL)
+            return false;
+        p = p->next;
+    }
+    if (p == NULL)
+        return false;
+    p->data = n;
+    return true;
+}
+bool Print_list(list *mylist)
+{
+    if (mylist == NULL)
+        return false;
+    list *p = mylist;
+    while (p->next != NULL)
+    {
+        printf("%d ", (p->next)->data);
+        p = p->next;
+    }
+    return true;
+}
+int Length_list(list *mylist)
+{
+    if (mylist == NULL)
+        return 0;
+    int length = 0;
+    list *p = mylist;
+    while (p->next != NULL)
+    {
+        length++;
+        p = p->next;
+    }
+    return length;
+}
+bool Is_empty_list(list *mylist)
+{
+    if (mylist == NULL)
+        return true;
+    else if (mylist->next == NULL)
+        return true;
+    else
+        return false;
+}
+bool Clear_list(list *mylist)
+{
+    if (mylist == NULL)
+        return false;
+    list *p = mylist->next;
+    list *q;
+    while (p != NULL)
+    {
+        q = p->next;
+        free(p);
+        p = q;
+    }
+    mylist->next = NULL;
+    return true;
+}
+int main()
+{
+    list *mylist;
+    Init_list(&mylist);
+}
